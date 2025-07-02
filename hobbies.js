74978f290bb3d6be6da5a0bf1cce76bbc373ab64
enririos <rios04enrique@gmail.com>
2025-07-03 00:31:19 +0200

    Más fixes

--- hobbies.js
+++ hobbies.js
@@ -1,9 +1,9 @@
-const hobbies =
-["jogging","cooking","diving","swimming","reading"];
+const hobbies = ["jogging","cooking","diving","swimming","reading"];
 
 exports.getHobbies = () => {
     return hobbies;
 }
+
 exports.getHobby = id => {
     if(id<1 || id > hobbies.length)
         return null;
