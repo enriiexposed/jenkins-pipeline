5df1b934b894c5aeae3eba8ad155c6fe536b73ef
enririos <rios04enrique@gmail.com>
2025-07-03 02:07:41 +0200

    .

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,11 @@ echo "Removing network test-net if it exists..."
 docker network rm test-net || true
 echo "Deploying app ($registry:$BUILD_NUMBER)..."
 docker network create test-net
-docker container run -d \
- --name api \
- --net test-net \
- $registry:$BUILD_NUMBER
+docker container run -d --name api --net test-net $registry:$BUILD_NUMBER
 
 # Logic to wait for the api component to be ready on port 3000
-read -r -d '' wait_for << 'EOF'
+read -d '' wait_for << EOF
+
 echo "Waiting for API to listen on port 3000..."
 
 while ! nc -z api 3000; do
@@ -20,14 +18,10 @@ while ! nc -z api 3000; do
 done
 
 echo "API ready on port 3000!"
+EOF
 
-docker container run --rm \
- --net test-net \
- node:12.10-alpine sh -c "$wait_for"
+docker container run --rm --net test-net node:12.10-alpine sh -c "$wait_for"
 
 echo "Smoke tests..."
 
-docker container run --name tester \
- --rm \
- --net test-net \
- enriii/node-docker sh -c "curl api:3000"
+docker container run --name tester --rm --net test-net enriii/node-docker sh -c "curl api:3000"
