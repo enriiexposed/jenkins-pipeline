3fbc6a8bf20f1d0b2151342a8883108ab966717b
enririos <rios04enrique@gmail.com>
2025-07-03 02:33:37 +0200

    .

--- deploy.sh
+++ deploy.sh
@@ -7,8 +7,6 @@ echo "Deploying app ($registry:$BUILD_NUMBER)..."
 docker network create test-net
 docker container run -d --name api --net test-net $registry:$BUILD_NUMBER
 
-docker container logs api
-
 # Logic to wait for the api component to be ready on port 3000
 read -d '' wait_for << EOF
 
